d9ff53ccae2995731fb5d87429b623301d2e38ac
Bongo Cat <saguayo@seas.upenn.edu>
2023-11-27 13:26:17 -0500

    working demo
    i h8 pkce

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -4,13 +4,52 @@
  * fetching the user's profile data, and updating the UI with the retrieved data.
  */
 
-// const clientId = "a509ebf3f1ed40f99b6c1c645a149032"; // Replace with your client id
-// const code = undefined;
-
 const client_id = 'a509ebf3f1ed40f99b6c1c645a149032';
 const client_secret = '58b18245f2624fa3bfae813e356cc346';
 var token: string;
 
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+  console.log("No code provided");
+  redirectToAuthCodeFlow(client_id);
+} else {
+  console.log("Code provided, fetching access token");
+  const accessToken = await getAccessToken(client_id, code);
+  console.log('Token', accessToken);
+  const profile = await fetchProfile(accessToken);
+  console.log('Profile Object:', profile);
+  populateUI(profile);
+}
+
+export async function redirectToAuthCodeFlow(clientId: string) {
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("scope", "user-read-private user-read-email");
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+export async function getAccessToken(clientId: string, code: string): Promise<string> {
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("client_secret", client_secret);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "http://localhost:5173/callback");
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params
+  });
+
+  const { access_token } = await result.json();
+  return access_token;
+}
 
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -18,14 +57,30 @@ var authOptions = {
     'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
   },
   form: {
-    grant_type: 'client_credentials'
+    grant_type: 'authorization_code',
+    code: 'AUTHORIZATION_CODE',  // Replace with the actual authorization code
+    redirect_uri: 'http://localhost:5173/callback'  // Replace with your actual redirect URI
   },
   json: true
 };
 
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//     token = body.access_token;
+//   }
+// });
+
+// Make the request to exchange the authorization code for an access token
 request.post(authOptions, function(error, response, body) {
   if (!error && response.statusCode === 200) {
     token = body.access_token;
+
+    // Now you can use the 'token' variable for making authenticated requests to the Spotify API.
+    // For example, you can call the 'fetchProfile' function.
+    fetchProfile(token);
+  } else {
+    // Handle errors
+    console.error('Error exchanging authorization code for access token:', error);
   }
 });
 
@@ -34,53 +89,67 @@ request.post(authOptions, function(error, response, body) {
  * @param accessToken The access token for authenticating the API request.
  * @returns A Promise that resolves to the user's profile data.
  */
-async function getProfile(accessToken: string): Promise<any> {
-    // let accessToken = localStorage.getItem('access_token');
-    accessToken = token;
 
-    const response = await fetch('https://api.spotify.com/v1/me', {
-        headers: {
-            Authorization: 'Bearer ' + accessToken
-        }
-    });
+// async function getProfile(accessToken: string): Promise<any> {
+//   const response = await fetch('https://api.spotify.com/v1/me', {
+//     headers: {
+//       Authorization: 'Bearer ' + accessToken
+//     }
+//   });
 
-    const data = await response.json
-    printData(data);
-}
+//   const data = await response.json();
+//   printData(data);
+// }
 
 /**
  * Fetches the user's Spotify profile using the provided access token.
  * @param token The access token for authenticating the API request.
  * @returns A Promise that resolves to the user's profile data.
  */
-async function fetchProfile(token: string): Promise<any> {
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+async function fetchProfile(token) {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
 
-    return await result.json();
+  if (result.ok) {
+    const data = await result.json();
+    console.log(data);
+    return data;  // Return the data
+  } else {
+    console.error("Error fetching profile:", result.statusText);
+    // You might want to throw an error or handle the error in some way
+  }
 }
 
-// if (!code) {
-//     redirectToAuthCodeFlow(clientId);
-// } else {
-//     const accessToken = await getAccessToken(clientId, code);
-//     const profile = await fetchProfile(accessToken);
-//     populateUI(profile);
-// }
-
-// async function redirectToAuthCodeFlow(clientId: string) {
-//     // TODO: Redirect to Spotify authorization page
-// }
-
-// async function getAccessToken(clientId: string, code: string) {
-//   // TODO: Get access token for code
-// }
-
+/**
+ * Fetches the user's Spotify profile using the provided access token.
+ * @param token The access token for authenticating the API request.
+ * @returns A Promise that resolves to the user's profile data.
+//  */
 // async function fetchProfile(token: string): Promise<any> {
-//     // TODO: Call Web API
+//     const result = await fetch("https://api.spotify.com/v1/me", {
+//         method: "GET", headers: { Authorization: `Bearer ${token}` }
+//     });
+
+//     return await result.json();
 // }
 
-// function populateUI(profile: any) {
-//     // TODO: Update UI with profile data
-// }
+/**
+ * Updates the UI with the user's profile information.
+ * @param profile The user's profile data.
+ */
+function populateUI(profile: any) {
+  document.getElementById("displayName")!.innerText = profile.display_name;
+  if (profile.images[0]) {
+      const profileImage = new Image(200, 200);
+      profileImage.src = profile.images[0].url;
+      document.getElementById("avatar")!.appendChild(profileImage);
+  }
+  document.getElementById("id")!.innerText = profile.id;
+  document.getElementById("email")!.innerText = profile.email;
+  document.getElementById("uri")!.innerText = profile.uri;
+  document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url")!.innerText = profile.href;
+  document.getElementById("url")!.setAttribute("href", profile.href);
+  document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+}
